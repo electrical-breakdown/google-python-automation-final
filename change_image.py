afd031a0995df4665612973434f5291cf790f7f7
Mike McGovern <mmcgovern5@gmail.com>
2020-06-16 12:47:15 -0600

    refactor change_image.py

--- change_image.py
+++ change_image.py
@@ -5,17 +5,17 @@ import os
 #set the source and destination folders, as well as the attributes we want for the file conversoin
 
 img_src = os.path.abspath(".\\supplier-data\\images")
-img_dest = ".\\supplier-data\\images"
+img_dest = os.path.abspath(".\\supplier-data\\images")
 dest_size = 600,400
 dest_format = "jpeg"
 
 
-for image in os.listdir(img_src):
-    img_path = os.path.join(img_src, image)
-  
+for img in os.listdir(img_src):
+    img_path = os.path.join(img_src, img)
+    img_base_name = img.split(".")[0]  
+    
     with open(img_path, "rb") as file:
-        print(file)
         
         im = Image.open(file)
         im.convert("RGB")
-        im.resize((dest_size)).save(dest + image, format=dest_format)
+        im.resize((dest_size)).im.save(os.path.join(img_dest, img_base_name) + ".JPEG", format=dest_format)
